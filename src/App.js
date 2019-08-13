404e32c13c64e069b334768105b3c4dbbf4a2af4
machariamwangi <you@example.com>
2019-08-13 12:42:38 -0400

    react-routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import PageWrapper from "./Components/PageWrapper";
 import Home from "./Components/Pages/Home";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <PageWrapper>
-          <Home />
-        </PageWrapper>
+        <Router>
+          <PageWrapper>
+            <Route path="/" component={Home} />
+          </PageWrapper>
+        </Router>
       </div>
     );
   }
